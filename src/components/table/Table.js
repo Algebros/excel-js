6ed54a7150a1c32273546b6cae27a3d87256473b
Algebros <sofroniy.m@gmail.com>
2020-06-27 18:36:49 +0300

    feat: implemented resize to table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,12 +1,13 @@
 import {ExcelComponent} from '../../core/ExcelComponent';
 import {createTable} from './table.template';
+import {$} from '../../core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   constructor($root) {
     super($root, {
-      listeners: ['mousemove'],
+      listeners: ['mousedown'],
     });
   }
 
@@ -14,7 +15,30 @@ export class Table extends ExcelComponent {
     return createTable();
   }
 
-  onMousemove() {
-    console.log(1);
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target);
+      const type = $resizer.data.resize;
+      const $parent = $resizer.closest('[data-type="resizable"]');
+      const coords = $parent.getCoords();
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
+
+      document.onmousemove = (e) => {
+        if (type === 'col') {
+          const delta = e.pageX - coords.right;
+          const value = coords.width + delta;
+          $parent.css({width: value + 'px'});
+          cells.forEach((el) => el.style.width = value + 'px');
+        } else {
+          const delta = e.pageY - coords.bottom;
+          const value = coords.height + delta;
+          $parent.css({height: value + 'px'});
+        }
+      };
+
+      document.onmouseup = () => {
+        document.onmousemove = null;
+      };
+    }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,23 +3,24 @@ const CODES = {
   Z: 90,
 };
 
-function toCell() {
+function toCell(_, index) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${index}"></div>
   `;
 }
 
-function toColumn(letter) {
-  return `<div class="column">
+function toColumn(letter, index) {
+  return `<div class="column" data-type="resizable" data-col="${index}">
     ${letter}
-    <div class="col-resize"></div>
+    <div class="col-resize" data-resize="col"></div>
   </div>`;
 }
 
 function createRow(content, index) {
-  const resize = index ? '<div class="row-resize"></div>' : '';
+  // eslint-disable-next-line max-len
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : '';
   return `
-   <div class="row">
+   <div class="row" data-type="resizable">
     <div class="row-info">
       ${index || ''}
       ${resize}
